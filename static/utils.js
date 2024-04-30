42a4fbe82e641bcf71b65859c0427282ca3fb84a
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2024-04-30 17:52:56 -0300

    Adding files

---
+++ static/utils.js
@@ -0,0 +1,67 @@
+function resize_canvas_to_display_size(canvas) {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    const displayWidth  = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+    
+    // Check if the canvas is not the same size.
+    const needResize = canvas.width  !== displayWidth || canvas.height !== displayHeight;
+    
+    if (needResize) {
+        // Make the canvas the same size
+        canvas.width  = displayWidth;
+        canvas.height = displayHeight;
+    }
+    
+    return needResize;
+}
+
+function create_shader(gl, type, source) {
+    var shader = gl.createShader(type);
+    
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function create_program(gl, vertex_shader, fragment_shader) {
+    var program = gl.createProgram();
+    
+    gl.attachShader(program, vertex_shader);
+    gl.attachShader(program, fragment_shader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function set_rectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]), gl.STATIC_DRAW);
+}
+
+function random_int(start, stop) {
+    return Math.floor(Math.random() * stop) + start;
+}
